a712c71c4d0823deabfa64af23127c8816056f1c
MwangiNelson <nelsonmwangi197@gmail.com>
2022-09-20 11:20:33 +0300

    changes

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState}from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import logo from "../assets/images/NELSON.png";
 import "./landing.css";
@@ -12,14 +12,19 @@ function Landing() {
           <h2>MWANGI NELSON</h2>
         </div>
       </nav>
-      <div className="parallax-bg w-100"></div>
+
       <div className="main w-100 jcc aic ud">
-        <div className="lr aic jcc g-1">
+        <div className="lr aic jcc g-2 bg-trans">
           <h1 className="intro">I am</h1> 
           <div className="name ud jcc aic">
             <h1 className="goat-name">NELSON</h1>
             <span className="white-hover"></span>
           </div>
+        </div>
+        <div className="hero-desc w-50 c-grey bg-trans">
+            <h4>
+                I am a software engineer specializing in web development. I also do lots of other stuff like eating.Designing beautiful web sites and creating solutions is art, I just can't help but <b className="red">love it</b>.
+            </h4>
         </div>
       </div>
     </>
